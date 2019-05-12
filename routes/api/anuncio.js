5589852c60c48a54ba8fa0ac98b3b19d5755ee0a
GuiFSs <devguisantos@gmail.com>
2019-05-12 02:23:11 -0300

    fix anuncios to return an array

--- routes/api/anuncio.js
+++ routes/api/anuncio.js
@@ -22,10 +22,7 @@ router.get('/', async (req, res) => {
     const anuncios = await Anuncio.find()
       .limit(limit)
       .exec();
-    if (anuncios.length === 0) {
-      return res.status(404).json({ anuncios: 'Nenhum anuncio encontrado' });
-    }
-    return res.status(200).json({ anuncios });
+    return res.status(200).json(anuncios);
   } catch (err) {
     res.status(400).json({ err });
   }
